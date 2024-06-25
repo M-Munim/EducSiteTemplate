8a103d5144096403745313cfcc17ef7b9b59e96e
munim <muhammadmunimoff330@gmail.com>
2024-06-25 17:33:10 +0500

    1:50

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
@@ -18,6 +20,9 @@ const App = () => {
         <Title subTitle='Gallery' title='Campus Photos' />
         <Campus />
         <Title subTitle='Testimonials' title='What Student Says' />
+        <Testimonials />
+        <Title subTitle='CONTACT US' title='Get In Touch' />
+        <Contact />
       </div>
     </>
   )
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className="contact-col">
+        <h3>Send us a message</h3>
+        <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
+
+        <ul>
+          <li>muhammadmunimoff330@gmail.com</li>
+          <li>+92 330 2021926</li>
+          <li>B17 Multigardens, Islamabad, Pakistan</li>
+        </ul>
+      </div>
+
+      <div className="contact-col">
+
+      </div>
+    </div>
+  )
+}
+
+export default Contact
